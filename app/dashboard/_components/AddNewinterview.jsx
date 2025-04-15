8f785c3568150fb914dc6f1ed06022ed1b7c67e7
Vishnu717v <vishnuvarthan.t2021csec@sece.ac.in>
2025-04-15 23:24:22 +0530

    fourth commit

--- app/dashboard/_components/AddNewinterview.jsx
+++ app/dashboard/_components/AddNewinterview.jsx
@@ -13,9 +13,24 @@ import {
 import { Ghost } from "lucide-react";
 import { Input } from "../../../components/ui/input";
 import { Textarea } from "../../../components/ui/textarea";
+import { chatSession } from "../../../utils/GeminiAIModel.js";
 
 export default function AddNewinterview() {
     const [openDailog,setOpenDailog]=useState(false)
+    const [jobPosition,setjobPosition]=useState();
+    const [jobDescripton,setjobDescription]=useState();
+    const [jobExperience,setjobExperience]=useState();
+
+    const onSubmit=async(e)=>{
+      e.preventDefault();
+      console.log(jobPosition,jobDescripton,jobExperience);
+      const questionCount = process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT || 5;
+      const InputPrompt = `job Position :${jobPosition}, job Description:${jobDescripton}, Years of Experience :${jobExperience}. Based on this info, give me ${questionCount} interview questions with answers in JSON format.`;
+            const result=await chatSession.sendMessage(InputPrompt);
+      console.log(result.response.text());
+      
+    }
+
   return (
     <div>
       <div className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all"
@@ -28,20 +43,25 @@ export default function AddNewinterview() {
             <DialogTitle className='font-bold text-2xl'>Tell us more about your job interviewing</DialogTitle>
             <DialogDescription>
             </DialogDescription>
-            <form>
+            <form onSubmit={onSubmit}>
               <div>
                 <h2>Add Details about your job position/role, Job description and years of experience </h2>
                 <div className="mt-7 my-3">
                     <label>Job Role/Job Position</label>
-                    <Input placeholder="Ex. Full Stack Developer" required/>
+                    <Input placeholder="Ex. Full Stack Developer" required
+                      onChange={(event)=>setjobPosition(event.target.value)}
+                    />
                 </div>
                 <div className="mt-7 my-3">
                     <label>Job Description/ Tech Stack</label>
-                    <Textarea placeholder="Ex. React,Aangular, NodeJs, Mysql etc" required />
+                    <Textarea placeholder="Ex. React,Angular, NodeJs, Mysql etc" required
+                    onChange={(event)=>setjobDescription(event.target.value)} />
                 </div>
                 <div className="my-3">
                     <label>Years Of Experience</label>
-                    <Input placeholder="Ex.5" type='Number'max={100} required/>
+                    <Input placeholder="Ex.5" type='Number'max={100} required
+                      onChange={(event)=>setjobExperience(event.target.value)}
+                    />
                 </div>
               </div>
               <div className="flex gap-5 justify-end">
