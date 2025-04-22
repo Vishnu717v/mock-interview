9b23be73dcb806a5142ab54088a87f4de76e2bc8
Vishnu717v <vishnuvarthan.t2021csec@sece.ac.in>
2025-04-22 22:33:08 +0530

    last commit

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return(
     <div>
       <h1>Welcome to Mock-Interview</h1>
+      <h2>Add /dashboard on the address bar to use the website</h2>
       
     </div>
   );
